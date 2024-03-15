fc14de70a4be1bbffe2635c2df8c75226eeb54d4
ottawacanad4 <20211784@nbsc.edu.ph>
2024-03-15 11:55:06 +0800

    third commit

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default function App() {
           <Stack.Screen
             name="Home"
             component={HomeScreen}
-            options={{ headerShown: false }}
+            options={{ title: "Team Vultures" }}
           />
           <Stack.Screen
             name="Login"
